b88c87e96cf6913280129ef6e0c6e69607e0f2cb
Conrad Mercer <conrad.mercer@gmail.com>
2020-10-14 16:39:26 +1030

    RAD-514: Add base clone method to component

--- src/component.ts
+++ src/component.ts
@@ -1,6 +1,9 @@
-
 import { Hashable } from './hashtable';
 
-export class Component extends Hashable {}
+export class Component extends Hashable {
+  clone() {
+    return Object.assign(Object.create(Object.getPrototypeOf(this)), this);
+  }
+}
 
-export type ComponentConstructor = { new(...args: any[]): Component };
+export type ComponentConstructor = { new (...args: any[]): Component };
